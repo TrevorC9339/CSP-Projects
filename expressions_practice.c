d4d4ef59e9134c07fc7a1c442a8aa58fe511b5ca
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-09-11 20:42:22 +0000

    started c expressions

--- expressions_practice.c
+++ expressions_practice.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
-    printf("Hello World!");
+    int power=(int)pow(2,4);
+    printf("%d\n", power);
+
+
+
+    printf("%s\n", "Hello World!");
+
+
+
+int value1;
+    value1=(7-24/8*4+6);
+    printf("%d\n", value1);
+
+int value2;
+    value2=(18/3-7+2*5);
+    printf("%d\n", value2);
+
+int value3;
+    value3=(6*4/12+72/8-9);
+    printf("%d\n", value3);
+
+int value4;
+    value4=((17-6/2)+4*3);
+    printf("%d\n", value4);
+
+int value5;
+    value5=(-2*(1*4-2/2)+(6+2-3));
+    printf("%d\n", value5);
+
+int value6;
+    value6=(18/3-7+2*5);
+    printf("%d\n", value6);
+
+int value7;
+    value7=(-1*((3-4*7)/5)-2*24/6);
+    printf("%d\n", value7);
+
+
+c
+
+int value8;
+    value8=((3*powerValue81/15)-(5-powerValue82));
+    printf("%d\n", value8);
+
+int powerValue91=(int)pow(1,4);
+int powerValue92=(int)pow(2,2);
+int powerValue93=(int)pow(3,3);
+int powerValue94=(int)pow(2,5);
+    
+int value9;
+    value9=()
+
     return 0;
+
+
+
 }

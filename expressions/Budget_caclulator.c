56fa9589353b04738aa5441969d11974a27ab8b5
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-09-12 20:25:22 +0000

    started budget

---
+++ expressions/Budget_caclulator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    float income, rent, transport, utilities, grocieries;
+    printf("This is going to calculate your budget for the month\n");
+    printf("How much do you make a month?\n");
+    scanf("%f", &income);
+    printf("How much do you pay for rent?\n");
+    scanf("%f", &rent);
+    printf("How much do your utilities cost?\n");
+    scanf("%f", &utilities);
+    printf("How much do you spend on gorceieres?\n");
+    scanf("%f", &groceries);
+    printf("How much do you spend on trasportation?\n")
+
+    printf("Hello World!");
+    return 0;
+ }
+    

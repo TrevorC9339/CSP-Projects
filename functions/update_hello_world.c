9debbf53707a8f26454dac2e413f6c052d353afc
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-09-25 20:41:06 +0000

    finnfihsed update hello world c

--- functions/update_hello_world.c
+++ functions/update_hello_world.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 char name;
-char john[5] = john;
+char john[5] = "john";
+char sam[5] = "sam";
+char joe[5] = "joe";
+char ty[5] = "Ty";
+char dan[5] = "Dan";
 
 void namer(char name[]){
-  char name = printf("Type a name:");
-  scanf("%s", name);
   printf("Hello %s!\n", name);
 
 
 }
 
 int main(void){
-    printf("Hello World!");
+    printf("Hello World!\n");
     
   namer("john");
+  namer("dan");
+  namer("sam");
+  namer("joe");
+  namer("ty");
      
 
 

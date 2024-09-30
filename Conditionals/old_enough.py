40868caaeb8ff5baa65de770dcff6061de66b68a
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-09-30 20:28:30 +0000

    finnished old enough? for python

---
+++ Conditionals/old_enough.py
@@ -0,0 +1,12 @@
+age = int(input("how old are you?"))
+
+if age >= 18:
+    print("you can vote")
+elif age >= 16:
+    print("You can get your drivers license")
+elif age >=15:
+    print("you can get your learners permit")
+elif age >=5:
+    print("You can start school")
+else:
+    print("go drink some milk baby")

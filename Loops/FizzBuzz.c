030214e09a9f07172234e9a15fe66583d644e0ea
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-10-10 18:41:29 +0000

    fizz buzz python done

--- Loops/FizzBuzz.c
+++ Loops/FizzBuzz.c
@@ -2,13 +2,15 @@
 
 int main(void){
     int i;
-        for (int i=1; i<=50; ++i){
-        }
-            if (i % 5 == 0){
-                printf("Buzz");
-            }else if (i % 3 == 0){
-                printf("Fizz");
-            }else print("%d\n", i)
-
+    for(i=1; i<=50; i++){
+        if(((i%3)||(i%5))== 0)
+            printf("FizzBuzz\n");
+        else if((i%3)==0)
+            printf("Fizz\n");
+        else if((i%5)==0)
+            printf("Buzz\n");
+        else
+            printf("%d\n",i);
+    }
     return 0;
 }

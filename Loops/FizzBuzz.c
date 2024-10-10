8c5ba5764262de450505fa4f63c353ae6d564632
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-10-10 20:13:29 +0000

    fiasdasdas

--- Loops/FizzBuzz.c
+++ Loops/FizzBuzz.c
@@ -3,11 +3,11 @@
 int main(void){
     int i;
     for(i=1; i<=50; i++){
-        if(((i%3)||(i%5))== 0)
+        if(i%3 == 0 && i%5 == 0)
             printf("FizzBuzz\n");
-        else if((i%3)==0)
+        else if(i%3==0)
             printf("Fizz\n");
-        else if((i%5)==0)
+        else if(i%5==0)
             printf("Buzz\n");
         else
             printf("%d\n",i);

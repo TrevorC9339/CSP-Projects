ccb8d1a64aef205b3f4f64e05af14b123f68e3d3
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-10-10 20:41:56 +0000

    sada

--- Loops/FizzBuzz.c
+++ Loops/FizzBuzz.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+int i, usr;
+char one[50], two[50], three[50];
 
+void delay(int sec){
+    int mili = 1000*sec;
+    clock_t start = clock();
+    while (clock()< start+mili);
+}
 int main(void){
-    int i;
-    for(i=1; i<=50; i++){
+printf("How high do you want the number?\n");
+scanf("%d", &usr);
+printf("give me a word\n");
+scanf("%s", one);
+printf("Give me another word\n");
+scanf("%s", two);
+strcat(three, two);
+strcat(one, three);
+    
+    for(i=1; i<=usr; i++){
         if(i%3 == 0 && i%5 == 0)
-            printf("FizzBuzz\n");
+            printf("%s\n", three);
         else if(i%3==0)
-            printf("Fizz\n");
+            printf("%s\n", one);
         else if(i%5==0)
-            printf("Buzz\n");
+            printf("%s\n", two);
         else
-            printf("%d\n",i);
+            printf("%d\n", i);
+            delay(500);
     }
     return 0;
 }

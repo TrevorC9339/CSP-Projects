d725cd485efe44daa8163ffb96fc12087e80c2b8
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-10-10 17:48:48 +0000

    fizzbuz c progress

---
+++ Loops/FizzBuzz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int i;
+        for (int i=1; i<=50; ++i){
+        }
+            if (i % 5 == 0){
+                printf("Buzz");
+            }else if (i % 3 == 0){
+                printf("Fizz");
+            }else print("%d\n", i)
+
+    return 0;
+}

fb63ae2dc6b05535df87fc1c1b7268978de049b6
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-11-04 19:46:08 +0000

    python final

--- final.py
+++ final.py
@@ -1,36 +1,86 @@
 import random
-wordList = ["hello", "tall", "exceptional", "defiant", "mouse"]
-def getWord():
-    word = random.choice(wordList)
-    return word.upper()
+wordList = ["exception", "straight", "replace", "direct", "temper", "cushion", "baseline", "savant"]
+word = random.choice(wordList)
 
-def play(word):
-    wordCompletion = "_" * len(word)
-    guessed = False
-    guessedLetters = []
-    guessedWords = []
-    tries = 6
-    print("this is hang man, you must guess teh word wihtout hanging the stickman. For every letter or words inccorect the stick man will gain a limb. Once he is complete you lose.")
-    print(displayHangman(tries))
-    print(wordCompletion)
-    while not guessed and tries > 0:
-        guess = input("guess a letter ot word").upper()
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessedLetters:
-                print("already guessed this letter")
-            elif guess not in word:
-                print("this is not in the word")
-                tires -= 1
-                guessedLetters.append(guess)
-            else:
-                print("Correct!", guess, "Is the word!")
-                multipleIndex = [i for i, letter in enumerate(word) if letter == guess]
-                for index in multipleIndex:
-                    wordAsList[index] = guess
-                    wordCompletion = "".join(wordAsList)
-                    if "_" not in wordCompletion:
-                        guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
-            
-        
-    
+allowedErrors = 6
+done = False
+guesses = []
+
+while not done:
+       for letter in word:
+              if letter.lower() in guesses:
+                     print(letter, end=" ")
+              else:
+                     print("_", end=" ")
+              print("")
+              guess = input("Guess a letter or word:")
+
+              guesses.append(guess.lower())
+
+              if guess.lower() not in word.lower():
+                     allowedErrors -= 1
+              if allowedErrors == 0:
+                     break
+              done = True
+              for letter in word:
+                     if letter.lower() not in word:
+                            done = False
+
+if done:
+       print(word, "is the correct answer!")
+else:
+       print("That is not correct", word, "is the correct answer.")
+
+
+
+
+
+def table(tries):
+        try0 = """ |--------|
+               |        
+               |         
+               |        
+               |
+               |_________"""
+        try1 = """ |--------|
+               |        O
+               |        
+               |        
+               |
+               |_________"""
+
+
+        try2 = """ |--------|
+               |        O
+               |        | 
+               |        
+               |
+               |_________"""
+        try3 = """ |--------|
+               |        O
+               |        |\
+               |        
+               |
+               |_________"""
+
+        try4 = """ |--------|
+               |        O
+               |       /|\
+               |        
+               |
+               |_________"""
+
+        try5 = """ |--------|
+               |        O
+               |       /|\
+               |         \
+               |
+               |_________"""
+
+        try6 = """ |--------|
+               |        O
+               |       /|\
+               |       / \
+               |
+               |_________"""
+        return[table]

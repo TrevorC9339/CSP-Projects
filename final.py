05614d2c56a21ad9e7055f333c20966325f1b452
TrevorC9339 <trevor.christensen@ucas-edu.net>
2024-11-08 19:46:02 +0000

    finalpy

--- final.py
+++ final.py
@@ -1,107 +1,41 @@
 import random
-wordList = ["exception", "straight", "replace", "direct", "temper", "cushion", "baseline", "savant"]
-word = random.choice(wordList)
-wordLength = len(word)
-allowedErrors = 6
-done = False
-guesses = []
-def table():
+import sys
 
-        if allowedErrors == 6: print(""" |--------|
-                                        |        
-                                        |         
-                                        |        
-                                        |
-                                        |_________""")
-        try1 = """ |--------|
-               |        O
-               |        
-               |        
-               |
-               |_________"""
+print("Welcome! Lets play Hangman.")
 
+wrongAttempt = 6
 
-        try2 = """ |--------|
-               |        O
-               |        | 
-               |        
-               |
-               |_________"""
-        try3 = """ |--------|
-               |        O
-               |        |\
-               |        
-               |
-               |_________"""
+wordList = ["time", "gap", "jump", "devolve", "weep"]
+secretWord = random.choice(wordList)
+print(secretWord)
+guesses = ''
+word = []
+for i in secretWord.strip("\n"):
+    word.append("_")
+while wrongAttempt > 0 :
+    wordStr = ''.join([str(elem) for elem in word])
 
-        try4 = """ |--------|
-               |        O
-               |       /|\
-               |        
-               |
-               |_________"""
+    print(wordStr)
+    none = 0
+    letter = input("Guess a word :\t")
+    for char in secretWord :
+        if char == letter :
+            print("Correct guess")
+            word.pop(none)
+            word.insert(none,char)
+            none += 1
+        else:
+              none += 1
+        if none == len(secretWord) :
+                     wrongAttempt -= 1
+                     print("Bad Luck. You have ",wrongAttempt," attempts left.")
+                     print("The secret word is ",secretWord)
+    none=0
 
-        try5 = """ |--------|
-               |        O
-               |       /|\
-               |         \
-               |
-               |_________"""
-
-        try6 = """ |--------|
-               |        O
-               |       /|\
-               |       / \
-               |
-               |_________"""
-        return[table]
-if wordLength == 3:
-       unguessed = "_ _ _"
-if wordLength == 4:
-       unguessed = "_ _ _ _"
-if wordLength == 5:
-       unguessed = "_ _ _ _ _ _"
-if wordLength == 6:
-       unguessed = "_ _ _ _ _ _"
-if wordLength == 7:
-       unguessed = "_ _ _ _ _ _ _"
-if wordLength == 8:
-       unguessed = "_ _ _ _ _ _ _ _"
-if wordLength == 9:
-       unguessed = "_ _ _ _ _ _ _ _ _"
-else:
-       print("")
-print(word)
-while not done:
-       for letter in word:
-              guess = input("Guess a letter or word:")
-
-              guesses.append(guess.lower())
-              if word.lower == guesses:
-                     done = True
-              if guess.lower()in word.lower():
-                     print("that is correct")
-                     print("you have", allowedErrors, "tries remaining")
-                     print(unguessed)
-                     wordLength -= 1
-                     if word[1] == guess:
-                            unguessed[1].replace("_", guess)
-                            print(guess + unguessed)
-                     if word[2] == guess:
-                            unguessed[3].replace("_", guess)
-                     if word[3] == guess:
-                            unguessed[5].replace("_", guess)
-              else:
-                     allowedErrors -= 1
-                     print("that is incorrect")
-                     print("you have", allowedErrors, "tries left")
-              if allowedErrors == 0:
-                     print("you have lost")
-                     print("play again if you wish")
-              break
-if done:
-       print(word, "is the correct answer!")
-       allowedErrors = 6
-else:
-       print("That is not correct", word, "is the correct answer.")
-
+    wordStr = ''.join([str(elem) for elem in word])
+    print(wordStr)
+    if wordStr == secretWord.strip("\n"):
+        print("you won")
+        sys.exit()
+if wrongAttempt == 0 :
+    print("You lost.")
